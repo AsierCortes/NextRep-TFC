96efc4454f880b2a6f3283ebe227d9521d8a8320
AsierCortes <asiercortes2006@gmail.com>
2026-02-09 18:33:13 +0100

    Commit Añadido Empezar

--- app/src/main/java/com/example/nextreptfc/MainActivity.kt
+++ app/src/main/java/com/example/nextreptfc/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.nextreptfc.ui.theme.NextRepTFCTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +28,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NextRepTFCTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NextRepTFCTheme {
-        Greeting("Android")
-    }
+fun Main(){
+    val navController = rememberNavController();
+
+    // Te dice en que pantalla estas actualmente (Es como un GPS)
+    val rutaActual = navController.currentBackStackEntryAsState().value?.destination?.route
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/nextreptfc/Vistas/Empezar.kt
@@ -0,0 +1,8 @@
+package com.example.nextreptfc.Vistas
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Empezar(){
+
+}
