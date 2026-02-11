c870fb312aa730a611b42d2214de8e795ca4a504
AsierCortes <asiercortes2006@gmail.com>
2026-02-11 17:24:26 +0100

    Estructura de carpetas bien organizadas y schema de colores hecha

--- app/src/main/java/com/example/nextreptfc/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nextreptfc/ui/theme/Theme.kt
@@ -11,33 +11,51 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+// MODO OSCURO (DARK MODE)
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = OrangePrimary,
+    onPrimary = DarkTextPrimary,
+
+    background = DarkBackground,
+    onBackground = DarkTextPrimary,
+
+    surface = DarkSurface,
+    onSurface = DarkTextPrimary,
+
+    surfaceVariant = DarkInput,
+    onSurfaceVariant = DarkTextSecondary,
+
+    error = RedError
 )
 
+// MODO CLARO (LIGHT MODE)
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = OrangePrimary,
+    onPrimary = LightTextPrimary,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = LightBackground,
+    onBackground = LightTextPrimary,
+
+    surface = LightSurface,
+    onSurface = LightTextPrimary,
+
+    surfaceVariant = LightInput,
+    onSurfaceVariant = LightTextSecondary,
+
+    error = RedError
 )
 
 @Composable
 fun NextRepTFCTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+
+    /*
+        Lo que hace dynamicColor es crear una paleta de colores basandose en el
+        fondo de pantalla del usuario. Por eso hay que desactivarlo, ya que no queremos
+        que haga eso
+     */
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
